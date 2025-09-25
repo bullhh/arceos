c578889f8a07d8b558bd28153dd79ae5d9741d43
YanLien <YJQ980314@outlook.com>
2025-09-25 11:13:59 +0800

    Adjust code formatting

--- modules/axruntime/src/lib.rs
+++ modules/axruntime/src/lib.rs
@@ -268,7 +268,7 @@ fn init_allocator() {
     let mut max_region_size = 0;
     let mut max_region_paddr = 0.into();
     let mut use_next_free = false;
-    
+
     for r in memory_regions() {
         if r.name == ".bss" {
             use_next_free = true;
